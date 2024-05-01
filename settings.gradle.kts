a62343eb6e365ad88f4deef93e2dacd1e9abbf16
Robert Novotny <novotnr0@gmail.com>
2024-05-01 23:23:55 +0200

    Gradle: Use 8.6, Kotlin DSL, and IntelliJ Platform Gradle Plugin 2.x

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "gitlab-quickmr-plugin"
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
